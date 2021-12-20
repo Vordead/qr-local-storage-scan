26d8bbecc8843a5536eb4743c943ffabff0512cc
Mohamad Hamade <mohamadhamade30@gmail.com>
2021-12-20 04:21:42 +0200

    done

--- report.js
+++ report.js
@@ -8,10 +8,20 @@
 const queryString = window.location.href;
 var foundToken = false;
 
-if (localStorage.getItem("196b95d9-c2bb-4549-8ee0-128a264df39f") != null) {
+if (localStorage.getItem("196b95d9-c2bb-4549-8ee0-128a264df39f") == null) {
     foundToken = true;
-    document.getElementById('isfound-text').textContent = localStorage.getItem("196b95d9-c2bb-4549-8ee0-128a264df39f");
+    document.getElementById('machine-name').textContent = "Machine name: FSQR12";
+    document.getElementById('condition').textContent = "Please select machine condition:";
+    document.getElementById('condition-choices1').textContent = "O DOWN";
+    document.getElementById('condition-choices2').textContent = "O LIMPING";
+    document.getElementById('condition-choices3').textContent = "O UP";
+    document.getElementById('details').textContent = "(Details textbox here)";
+    document.getElementById('submit-button').textContent = "(Submit button here)";
+
+
+
+
 }
-else{
+else {
     document.getElementById('isfound-text').textContent = "Unrecognized user!\nPlease refer to IT"
 }
