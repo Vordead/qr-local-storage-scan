3d92d65168396418216097c69363bfb4a4c99d56
Mohamad Hamade <mohamadhamade30@gmail.com>
2021-12-21 04:35:12 +0200

    navigation to report.html if found token

--- report.js
+++ report.js
@@ -6,22 +6,13 @@
 
 
 const queryString = window.location.href;
-var foundToken = false;
+var foundToken = false; //to be used later
 
-if (localStorage.getItem("196b95d9-c2bb-4549-8ee0-128a264df39f") != null) {
+if (localStorage.getItem("196b95d9-c2bb-4549-8ee0-128a264df39f") == null) {
     foundToken = true;
-    document.getElementById('machine-name').textContent = "Machine name: FSQR12";
-    document.getElementById('condition').textContent = "Please select machine condition:";
-    document.getElementById('condition-choices1').textContent = "O DOWN";
-    document.getElementById('condition-choices2').textContent = "O LIMPING";
-    document.getElementById('condition-choices3').textContent = "O UP";
-    document.getElementById('details').textContent = "(Details textbox here)";
-    document.getElementById('submit-button').textContent = "(Submit button here)";
-
-
-
-
+    window.location = "report.html";
 }
 else {
-    document.getElementById('isfound-text').textContent = "Unrecognized user!\nPlease refer to IT"
+    document.getElementById('h1').textContent = "Unrecognized user!";
+    document.getElementById('h2').textContent = "Please refer to IT";
 }
