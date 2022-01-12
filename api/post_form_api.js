5726b21f1627dbbc939054cc8b283f3a05737a0f
Mohamad Hamade <mohamadhamade30@gmail.com>
2022-01-13 00:55:24 +0200

    changes to api request

--- api/post_form_api.js
+++ api/post_form_api.js
@@ -1,6 +1,13 @@
-const handleSubmit = (e) => {
+const handleSubmit = () => {
+
+
+
+    /*
+
+
+
     console.log("test")
-    e.preventDefault();
+    // e.preventDefault();
     const form_request = {
         "equipmentId": parseInt(document.getElementById("equipment-id").innerText),
         "createdById": parseInt(localStorage.getItem("196b95d9-c2bb-4549-8ee0-128a264df39f")),
@@ -21,6 +28,62 @@ const handleSubmit = (e) => {
         console.log(response.body);
         console.log('Success');
     })
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        "equipmentId": parseInt(document.getElementById("equipment-id").innerText),
+        "createdById": parseInt(localStorage.getItem("196b95d9-c2bb-4549-8ee0-128a264df39f")),
+        "title": document.getElementById("priority").value,
+        "description": document.querySelector('textarea').value,
+        "isSafetyIssue": document.getElementById('safe-issue').checked,
+        "equipmentConditionId": document.querySelector('input[name="condition"]:checked').value,
+        "workOrderNumber": "WO12345678"
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch("https://phoneservices.azurewebsites.net/api/WorkOrders?code=bP3n9xcaKQ/l5xPKNjS5r1nT/EwgWImkj6WfDHwDZbWE5UBH220sAg==", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
