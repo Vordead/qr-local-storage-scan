7ef1ca833d4f6245a7284b5b7381e58e14bdf0ec
Mohamad Hamade <mohamadhamade30@gmail.com>
2022-01-15 18:18:08 +0200

    fixed a bug related to displaying equipment-id

--- report.js
+++ report.js
@@ -8,10 +8,22 @@
 const queryString = window.location.href;
 
 
-var foundToken = false; //to be used later
 
 if (localStorage.getItem("196b95d9-c2bb-4549-8ee0-128a264df39f") != null) {
     foundToken = true;
+    console.log(queryString);
+    var index;
+    for (let i = 0; i < queryString.length; i++) {
+        console.log(queryString[i]);
+        if (queryString[i] == "=") {
+            index = i;
+            console.log(index)
+            break;
+        }
+    }
+    var equipment_id = queryString.substring(index + 1);
+    console.log(equipment_id);
+    localStorage.setItem('equipment-id', equipment_id);
     window.location = "report.html";
 } else {
     document.getElementById('h1').textContent = "Unrecognized user!";
