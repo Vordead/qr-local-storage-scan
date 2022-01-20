69b3af12149e2c687473ba85ff90fa6e69361dbe
Mohamad Hamade <mohamadhamade30@gmail.com>
2022-01-20 21:01:13 +0200

    fixed api

--- report.js
+++ report.js
@@ -8,8 +8,6 @@
 
 const queryString = window.location.href;
 
-
-
 if (localStorage.getItem("196b95d9-c2bb-4549-8ee0-128a264df39f") != null) {
     foundToken = true;
     console.log(queryString);
@@ -24,23 +22,7 @@ if (localStorage.getItem("196b95d9-c2bb-4549-8ee0-128a264df39f") != null) {
     }
     var equipment_id = queryString.substring(index + 1);
     console.log(equipment_id);
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
-
-    fetch("https://phoneservices.azurewebsites.net/api/equipment?code=yMYfkKhLd/SVVDNdTuKCKRj0d973Wd2UurFka1IwPvD04W9ACd1hEA==&id=" + equipment_id, requestOptions)
-        .then(response => response.text())
-        .then(result => {
-            console.log(result);
-            document.getElementById('equipment-id').innerHTML = result;
-        })
-        .catch(error => {
-            console.log('error', error);
-            document.getElementById('equipment-id').innerHTML = result;
-        });
-
-    window.location = "report.html";
+    window.location = "report.html?id=" + equipment_id;
 } else {
     document.getElementById('h1').textContent = "Unrecognized user!";
     document.getElementById('h2').textContent = "Please refer to IT";
